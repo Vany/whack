1f6e32d4c5a6d4ed6db457fbf4d32b00bc7a8fd3
vany <ivan.serezhkin@insolar.io>
2019-06-19 10:29:13 +0300

    httpserver

---
+++ agents/http/server.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"os/exec"
+	"strconv"
+	"time"
+)
+
+
+var last_data = time.Now()
+var cache_time = time.Duration(10 *time.Second)
+var data_cache []byte
+var command = "../api_agent.py"
+
+func MHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write(data_cache)
+}
+
+func fetcher() {
+	out, err := exec.Command("python3" , command).Output()
+	if err!=nil {
+		data_cache = []byte(`[{"name":"error", "bonus":10, "latitude": 0, "longitude": 0}]`)
+		return
+	}
+	data_cache = out
+}
+
+
+func main() {
+
+	go func() {
+		for {
+			fetcher()
+			time.Sleep(cache_time)
+		}
+	}()
+
+	http.HandleFunc("/", MHandler)
+	port, ok := os.LookupEnv("HTTP_PORT")
+	if !ok { port = "8010"}
+
+	_, err := strconv.Atoi(port)
+	if err != nil {
+		panic("not numeric port")
+	}
+
+	err = http.ListenAndServe(":" + port, nil) // задаем слушать порт
+	if err != nil {
+		panic("ListenAndServe: "+ err.Error())
+	}
+}
