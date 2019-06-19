e811c825603fc88822b4dc23d45f1e10d8b43e01
Andrey Bronin <andrey.bronin@insolar.io>
2019-06-19 03:01:02 +0300

    add charger markers

--- explorer/src/app/home/home.page.ts
+++ explorer/src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {latLng, tileLayer} from 'leaflet';
+import {icon, latLng, marker, tileLayer} from 'leaflet';
 
 @Component({
   selector: 'app-home',
@@ -8,14 +8,56 @@ import {latLng, tileLayer} from 'leaflet';
 })
 export class HomePage {
 
+  layers = [];
   options = {
     layers: [
       tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
     ],
-    zoom: 5,
-    center: latLng(46.879966, -121.726909)
+    zoom: 11,
+    center: latLng(52.520008, 13.404954),
+
   };
 
   constructor() {}
+
+  ngOnInit() {
+    const m = marker([52.520008, 13.404954], {
+      title: 'charger_1',
+      alt: 'charger_1',
+      clickable: true,
+      riseOnHover: true,
+      icon: icon({
+        iconSize: [ 25, 41 ],
+        iconAnchor: [ 13, 41 ],
+        iconUrl: 'assets/marker-icon.png',
+        shadowUrl: 'assets/marker-shadow.png'
+      })
+    });
+
+    this.layers.push(m);
+  }
+
+  click(event) {
+    console.log(event);
+
+    const m = marker([52.530108, 13.404954], {
+      title: 'charger_2',
+      alt: 'charger_2',
+      clickable: true,
+      riseOnHover: true,
+      icon: icon({
+        iconSize: [ 25, 41 ],
+        iconAnchor: [ 13, 41 ],
+        iconUrl: 'assets/marker-icon.png',
+        shadowUrl: 'assets/marker-shadow.png'
+      })
+    });
+
+    // m.bindPopup('<ion-button size="small" >charge</ion-button>');
+
+
+    this.layers.push(m);
+  }
+
 
 }
